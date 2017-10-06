f235237e99f2314be8536753e283754a0b732edb
Mark DeLillo <mdelillo@pivotal.io>
2017-10-06 13:34:23 -0400

    init repo
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

---
+++ jobs/windows2016fs/templates/pre-start.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+$layersDir = "c:\var\vcap\data\windows2016fs\layers"
+$topLayer = (c:\var\vcap\packages\extract\extract.exe c:\var\vcap\packages\windows2016fs\windows2016fs.tgz $layersDir)
+if ($LASTEXITCODE -ne 0) {
+  Exit $LASTEXITCODE
+}
+
+$rootfsPackage="C:\var\vcap\packages\windows2016fs"
+$rootfsSymlink="$rootfsPackage\rootfs"
+
+if ((Test-Path $rootfsSymlink) -eq $true) {
+  cmd.exe /c "rmdir $rootfsSymlink"
+  if ($LASTEXITCODE -ne 0) {
+    Write-Error "Failed to remove old symlink"
+  }
+}
+
+cmd.exe /c "mklink /D $rootfsSymlink $topLayer"
+if ($LASTEXITCODE -ne 0) {
+  Write-Error "Failed to create symlink"
+}
